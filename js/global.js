0fed84e21a2e35be282bc4dc6710ffa7ab067bc1
gscoon <gscoon@gmail.com>
2016-04-14 10:41:25 -0400

    removed node_modules

--- js/global.js
+++ js/global.js
@@ -38,16 +38,3 @@ Date.prototype.formatMYSQL = function(includeTime){
 
     return retString;
 }
-
-// First, checks if it isn't implemented yet.
-if (!String.prototype.format) {
-    String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
-        });
-    };
-}
